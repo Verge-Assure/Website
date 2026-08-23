9b833d7d2e16d4151fa122d0d61a05f54d137a35
salonimaheshwari98 <saloni.jaju.95@gmail.com>
2026-08-23 13:27:39 +0530

    Removed team section

--- verge_assure-landing_page-main/src/App.tsx
+++ verge_assure-landing_page-main/src/App.tsx
@@ -55,7 +55,7 @@ export function App() {
           <ServicesSection />
           <WhyVergeAssure />
           <ProductsSection />
-          <TeamSection isHomeSection={true} />
+          {/* <TeamSection isHomeSection={true} /> */}
           <CtaSection />
         </>
       )}
@@ -63,7 +63,7 @@ export function App() {
       {currentPath === '/services' && <ServicesSection />}
       {currentPath === '/products' && <ProductsSection />}
       {currentPath === '/about' && <WhyVergeAssure />}
-      {currentPath === '/team' && <TeamSection />}
+      {/* {currentPath === '/team' && <TeamSection />} */}
       {currentPath === '/contact' && <CtaSection />}
       {currentPath === '/book-demo' && <BookDemoSection />}
       {currentPath === '/thank-you' && <ThankYouSection />}
